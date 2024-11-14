35b5c6f3cf65c2bccfccf50885021dc22eff6ba4
Łukasz Lebiecki <lukasz.lebiecki@gmail.com>
2024-11-14 23:41:51 +0100

    create first button

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +10,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setText("zmienilem tekst");
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-    }
 }
