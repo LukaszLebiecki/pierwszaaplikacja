b75638d2dcfa259dec1af96a5cf4f4c403e172da
Łukasz Lebiecki <lukasz.lebiecki@gmail.com>
2024-11-18 22:02:50 +0100

    simple exercise

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,11 +17,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        Button okButton = findViewById(R.id.ok);
 
-        binding.button.setText("zmienilem tekst");
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText nameTextView = findViewById(R.id.name);
+                RadioButton ganderMale = findViewById(R.id.male);
+                RadioButton ganderFemale = findViewById(R.id.female);
+
+                String name = nameTextView.getText().toString();
+                String gander = "";
+
+                if (ganderMale.isChecked()) {
+                    gander = "Facet! ";
+                }
+
+                if (ganderFemale.isChecked()) {
+                    gander = "Kobieta! ";
+                }
+
+                Toast.makeText(getApplicationContext(), gander + name, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
 }
